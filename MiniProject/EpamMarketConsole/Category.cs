8c359b9cc020ca081f5457d9619685cb20e9a101
Sachin Sharma <sachinsharma9998@gmail.com>
2020-01-22 00:06:41 +0530

    MiniProject initial commit

---
+++ MiniProject/EpamMarketConsole/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamMarketConsole
+{
+    class Category
+    {
+        private string name;
+        private List<ProductModel> categoryProducts;
+
+        public Category(string name)
+        {
+            this.Name = name;
+        }
+        public Category(string name,List<ProductModel> categoryProducts)
+        {
+            this.Name = name;
+            this.CategoryProducts = categoryProducts;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public List<ProductModel> CategoryProducts { get => categoryProducts; set => categoryProducts = value; }
+    }
+}
---
+++ MiniProject/EpamMarketConsole/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamMarketConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ProductModel> mobileProducts = generateMobileProducts();
+            Category mobiles = new Category("Mobiles/Tablets",mobileProducts);
+
+            List<ProductModel> fashionProducts = generateFashionProducts();
+            Category fashion = new Category("Fashion", mobileProducts);
+
+            List<Category> categories = new List<Category>();
+            categories.Add(mobiles);
+            categories.Add(fashion);
+
+            View view = new View(categories);
+            view.ShowCategories();
+            int response = int.Parse(Console.ReadLine());
+            view.showProducts(response);
+
+        }
+
+        
+
+        private static List<ProductModel> generateMobileProducts()
+        {
+            ProductModel product = new ProductModel("OnePlus 7t", "", 37999);
+            ProductModel product2 = new ProductModel("Samsung A7s", "", 17999);
+            ProductModel product3 = new ProductModel("Iphone XR", "", 49999);
+            List<ProductModel> products = new List<ProductModel>();
+            products.Add(product);
+            products.Add(product2);
+            products.Add(product3);
+
+            return products;
+        }
+
+        private static List<ProductModel> generateFashionProducts()
+        {
+            ProductModel product = new ProductModel("Polo tshirt", "", 599);
+            ProductModel product2 = new ProductModel("White shirt", "", 699);
+            ProductModel product3 = new ProductModel("Blue denim", "", 1250);
+            List<ProductModel> products = new List<ProductModel>();
+            products.Add(product);
+            products.Add(product2);
+            products.Add(product3);
+
+            return products;
+        }
+    }
+}
