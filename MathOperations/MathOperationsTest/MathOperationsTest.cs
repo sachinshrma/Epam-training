b04ae5357d867bb3ec8e6a19f8ce9fbcc8ac893f
Sachin Sharma1 <Sachin_Sharma1@epam.com>
2020-01-23 17:06:31 +0530

    Committing Code for unit test

---
+++ MathOperations/MathOperationsTest/MathOperationsTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathOperationsTest
+{
+    [TestClass]
+    public class MathOperationsTest
+    {
+        [TestMethod]
+        public void AddNumbersTest()
+        {
+            #region "Arrange"
+            int number1 = 2, number2 = 4;
+            MathOperations.MathOperations objMathOperation = new MathOperations.MathOperations(number1, number2);
+            #endregion
+
+            #region "Act"
+            double responseResult = objMathOperation.AddNumbers();
+            #endregion
+
+            #region "Assert"
+            Assert.AreEqual(6, responseResult);
+            #endregion
+        }
+
+        [TestMethod]
+        public void SubtractNumbersTest()
+        {
+            #region "Arrange"
+            int number1 = 2, number2 = 4;
+            MathOperations.MathOperations objMathOperation = new MathOperations.MathOperations(number1, number2);
+            #endregion
+
+            #region "Act"
+            double responseResult = objMathOperation.SubtractNumbers();
+            #endregion
+
+            #region "Assert"
+            Assert.AreEqual(-2, responseResult);
+            #endregion
+        }
+
+        [TestMethod]
+        public void MultiplyNumbersTest()
+        {
+            #region "Arrange"
+            int number1 = 2, number2 = 4;
+            MathOperations.MathOperations objMathOperation = new MathOperations.MathOperations(number1, number2);
+            #endregion
+
+            #region "Act"
+            double responseResult = objMathOperation.MultiplyNumbers();
+            #endregion
+
+            #region "Assert"
+            Assert.AreEqual(8, responseResult);
+            #endregion
+        }
+
+        [TestMethod]
+        
+        public void DivideNumbersTest()
+        {
+            #region "Arrange"
+            int number1 = 2, number2 = 4, number3=0;
+            MathOperations.MathOperations objMathOperation = new MathOperations.MathOperations(number1, number2);
+            MathOperations.MathOperations objMathOperation2 = new MathOperations.MathOperations(number1, number3);
+            #endregion
+
+            #region "Act"
+            double responseResult = objMathOperation.DivideNumbers();            
+            #endregion
+
+            #region "Assert"
+            Assert.AreEqual(0.5, responseResult);
+            Assert.ThrowsException<DivideByZeroException>(()=> objMathOperation2.DivideNumbers());            
+            #endregion
+        }
+    }
+}
---
+++ MathOperations/MathOperationsTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MathOperationsTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("MathOperationsTest")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("93720934-ed3f-4b44-ae08-c04f0d6861c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
