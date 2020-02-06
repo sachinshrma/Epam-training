854473857e0036a04be98ee2607c9130d532fd10
Sachin_Sharma1 <Sachin_Sharma1@epam.com>
2020-02-06 17:57:58 +0530

    Registration Webapp initial commit

---
+++ StudentRegisterationWebApp/StudentRegisterationWebApp/DAO/DummyCountryData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentRegisterationWebApp.DAO
+{
+    public class DummyCountryData
+    {
+        Dictionary<string, List<string>> _countryToState = new Dictionary<string, List<string>>();
+        Dictionary<string, List<string>> _sateToCity = new Dictionary<string, List<string>>();
+
+        public DummyCountryData()
+        {
+            _countryToState.Add("India", new List<string>() { "Andhra Pradesh", "Uttar Pradesh", "Haryana", "Punjab" });
+
+            _sateToCity.Add("Andhra Pradesh", new List<string>() { "Vijayawada", "Guntur", "Kadapa", "Tenali" });
+            _sateToCity.Add("Uttar Pradesh", new List<string>() { "Kanpur", "Lucknow", "Ghaziabad" });
+            _sateToCity.Add("Haryana", new List<string>() { "Ambala", "Karnal" });
+            _sateToCity.Add("Punjab", new List<string>() { "Mohali", "Rajpura" });
+
+
+            _countryToState.Add("China", new List<string>() { "Fujian", "Gansu", "Guangdong" });
+            _sateToCity.Add("Fujian", new List<string>() { "Shaowu", "Shishi", "Wuyishan" });
+            _sateToCity.Add("Gansu", new List<string>() { "Baiyin", "Dingxi", "Dunhuang" });
+            _sateToCity.Add("Guangdong", new List<string>() { "Foshan", "Gaozhou", "Shishi" });
+
+            _countryToState.Add("Nepal", new List<string>() { "Bagmati", "Gandaki", "Sudurpashchim" });
+
+            _sateToCity.Add("Bagmati", new List<string>() { "Kathmandu", "Lalitpur", "Wuyishan" });
+            _sateToCity.Add("Gandaki", new List<string>() { "Pokhara", "Baiyin", "Gansu" });
+            _sateToCity.Add("Sudurpashchim", new List<string>() { "Mohali", "Karnal", "Vijayawada" });
+        }
+
+
+
+        public List<string> getCountries() => _countryToState.Keys.ToList();
+
+        public List<string> getStates(string countryName) => _countryToState[countryName];
+
+        public List<string> getCities(string stateName) => _sateToCity[stateName];
+
+
+    }
+}
---
+++ StudentRegisterationWebApp/StudentRegisterationWebApp/RegisterationForm.aspx.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using StudentRegisterationWebApp.DAO;
+namespace StudentRegisterationWebApp
+{
+    public partial class RegisterationForm : System.Web.UI.Page
+    {
+        private DummyCountryData _objDummyData;
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            _objDummyData = new DummyCountryData();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                var _allCountries = _objDummyData.getCountries();
+                Ddl_Country.Items.Add("Select Country");
+                Ddl_State.Items.Add("Select State");
+                Ddl_City.Items.Add("Select City");
+                foreach (var country in _allCountries)
+                {
+                    Ddl_Country.Items.Add(country);
+                }
+            }
+        }
+
+        protected void Ddl_Country_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Ddl_Country.SelectedIndex > 0)
+            {
+                Ddl_State.Items.Clear();
+                Ddl_City.Items.Clear();
+                var _allState = _objDummyData.getStates(Ddl_Country.SelectedValue);
+                Ddl_State.Items.Add("Select State");
+                Ddl_City.Items.Add("Select City");
+                foreach (var state in _allState)
+                {
+                    Ddl_State.Items.Add(state);
+                }
+            }
+            else
+            {
+                Ddl_State.Items.Clear();
+                Ddl_City.Items.Clear();
+                Ddl_State.Items.Add("Select State");
+                Ddl_City.Items.Add("Select City");
+
+            }
+        }
+
+        protected void Ddl_State_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Ddl_State.SelectedIndex > 0)
+            {
+                Ddl_City.Items.Clear();
+                var _allCities = _objDummyData.getCities(Ddl_State.SelectedValue);
+                foreach (var city in _allCities)
+                {
+                    Ddl_City.Items.Add(city);
+                }
+            }
+            else
+            {
+                Ddl_City.Items.Clear();
+                Ddl_City.Items.Add("Select City");
+            }
+
+        }
+
+        protected void Ddl_City_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
